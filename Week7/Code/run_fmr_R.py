db3e77c0369defa5b3235f210f251e9909578abb
gregiee <greggaryyang@gmail.com>
2019-11-14 14:19:07 +0000

    update some files.

--- Week7/Code/run_fmr_R.py
+++ Week7/Code/run_fmr_R.py
@@ -11,17 +11,20 @@ import ipdb
 #If the stdout argument was PIPE, this attribute is a file object that provides output from the child process. Otherwise, it is None.
 #Popen.stderr
 #If the stderr argument was PIPE, this attribute is a file object that provides error output from the child process. Otherwise, it is None.
-process = subprocess.Popen("Rscript fmr.R", stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
-#ipdb.set_trace()
+p = subprocess.Popen("Rscript fmr.R", stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+
 
 #Popen.communicate(input=None)
 #Interact with process: Send data to stdin. Read data from stdout and stderr, until end-of-file is reached. Wait for process to terminate. The optional input argument should be a string to be sent to the child process, or None, if no data should be sent to the child.
 #communicate() returns a tuple (stdoutdata, stderrdata).
-stdout, stderr = process.communicate()
+stdout, stderr = p.communicate()
 
+# ipdb.set_trace()
 if stderr:
-    print("Raise error:\n")
+	print("===============")
+    print("Raise error:")
     print(stderr.decode())
 else:
-    print("fmr.R is executed:\n")
+	print("===============")
+    print("fmr.R is executed:")
     print(stdout.decode())
